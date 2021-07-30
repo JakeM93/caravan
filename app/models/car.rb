83469a6d5e2c9a3fcf2cf4d427d41dcff5550327
Jake Monger <jakemonger18@gmail.com>
2021-07-30 15:39:34 +0100

    Added car model validations for new columns

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,9 +1,11 @@
 class Car < ApplicationRecord
   belongs_to :user
 
-  validates :model, :make, presence: true, length: { maximum: 50 }
+  validates :model, :make, :fuel_type, presence: true, length: { maximum: 50 }
   validates :location, presence: true, length: { maximum: 100 } # full address?
-  validates :reg_number, presence: true, length: { maximum: 10 }
+  validates :reg_number, :transmission, presence: true, length: { maximum: 10 }
   validates :price, presence: true
   validates :user_id, presence: true
+  validates :seats, presence: true, inclusion: { in: 1..8, message: 'Invalid Option please choose between 1 to 8' }
+
 end
