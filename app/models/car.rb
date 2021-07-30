1ba197ff39d04b0670c565a185f08ed1fe3a8363
Jake Monger <jakemonger18@gmail.com>
2021-07-30 15:05:57 +0100

    Added Car Validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,9 @@
 class Car < ApplicationRecord
   belongs_to :user
+
+  validates :model, :make, presence: true, length: { maximum: 50 }
+  validates :location, presence: true, length: { maximum: 100 } # full address?
+  validates :reg_number, presence: true, length: { maximum: 10 }
+  validates :price, presence: true
+  validates :user_id, presence: true
 end
