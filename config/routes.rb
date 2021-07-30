e8748f3479f4ecbb3f9d4de7917f47c511e615f0
Jake Monger <jakemonger18@gmail.com>
2021-07-30 16:18:37 +0100

    Added cars routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :cars # All Routes for now untill other models and routes are defined
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
