26cc8a7d6fe03eccab429f4f781295763fd95475
Anne Wong <directtoanne@gmail.com>
2021-07-31 16:55:01 +0100

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+new_user = User.new(email: Faker::Internet.email, first_name: "123456", last_name: "123456", password: "123456")
+new_user.save!
+
 100.times do 
-  Car.create(model: Faker::Company.name, make: Faker::Company.suffix, fuel_type: Faker::Company.industry, location: Faker::Address.city, reg_number: Faker::Number.number(digits: 10), price: Faker::Number.decimal(l_digits: 2), user_id: Faker::Number.decimal_part(digits: 2), seats: Faker::Number.between(from: 1, to: 10))
+  car = Car.create(model: Faker::Company.name, user_id: new_user.id, transmission: Faker::Number.number(digits: 10), make: Faker::Company.suffix, fuel_type: Faker::Company.industry, location: Faker::Address.city, reg_number: Faker::Number.number(digits: 10), price: Faker::Number.decimal(l_digits: 2), seats: Faker::Number.between(from: 1, to: 8))
+  car.save!
   puts "making car user"
 end
