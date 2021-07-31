caddd66c673aa27a4e8bfcaa74a7dff69290dabb
Kris Saykova <kssaykova@gmail.com>
2021-07-31 14:21:17 +0300

    Add new routes for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :cars # All Routes for now untill other models and routes are defined
+  resources :cars do
+    resources :bookings, only: [ :new, :create, :index, :show ]
+  end
+  # All Routes for now untill other models and routes are defined
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
