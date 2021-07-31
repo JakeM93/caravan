76db64fd985362a87f2772d61dab475a101e8818
Kris Saykova <kssaykova@gmail.com>
2021-07-31 14:21:59 +0300

    Add migration for bookings table

---
+++ db/migrate/20210731110550_create_bookings.rb
@@ -0,0 +1,15 @@
+class CreateBookings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookings do |t|
+      t.datetime :start_date
+      t.datetime :end_date
+      t.string :location
+      t.decimal :total_cost
+      t.boolean :active
+      t.references :car, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
