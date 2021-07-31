612856d9c7564b483b1c851de193f18a002d87bd
Anne Wong <directtoanne@gmail.com>
2021-07-31 15:05:08 +0100

    booking controller params defined

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,13 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @car = Car.find(params[:car_id])
   end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.car = @car(params: car_id)
-    @booking.user = @user
+    @booking.user = @user(params: user_id)
     if @booking.after_save 
       redirect to booking_path
     else
@@ -21,5 +22,11 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :location, :car_id)
   end
 end
