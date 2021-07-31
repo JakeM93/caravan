e1cfa41058c9a7de5a8590569311095f36ee6e96
Anne Wong <directtoanne@gmail.com>
2021-07-31 11:38:04 +0100

    added new seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do 
+  Car.create(model: Faker::Company.name, make: Faker::Company.suffix, fuel_type: Faker::Company.industry, location: Faker::Address.city, reg_number: Faker::Number.number(digits: 10), price: Faker::Number.decimal(l_digits: 2), user_id: Faker::Number.decimal_part(digits: 2), seats: Faker::Number.between(from: 1, to: 10))
+  puts "making car user"
+end
