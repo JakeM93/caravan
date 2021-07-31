4a151b3d01d852c8c64547cf8070a49d56349d78
Anne Wong <directtoanne@gmail.com>
2021-07-31 13:08:04 +0100

    added destroy to booking controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :cars do
-    resources :bookings, only: [ :new, :create, :index, :show ]
+    resources :bookings, only: [ :new, :create, :index, :show, :destroy ]
   end
   # All Routes for now untill other models and routes are defined
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
