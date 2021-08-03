32323019a1a2dcb565b7bfd72df3e4daa69e8e1a
Anne Wong <directtoanne@gmail.com>
2021-08-03 20:43:30 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
     collection do
       get :my_cars
     end
-    resources :bookings, only: [ :new, :create, :index, :show, :destroy ]
+    resources :bookings, only: [ :new, :create, :index, :show, :destroy ] do
+      collection do
+        get :my_bookings
+      end
+    end
   end
   # All Routes for now untill other models and routes are defined
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
