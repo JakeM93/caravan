240d8132891f2160708c023ddaaabec8d7562953
Jake Monger <jakemonger18@gmail.com>
2021-08-03 19:51:15 +0100

    Updated Routes and My_Car view from Kris Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  
-  get '/cars/my_cars', to: 'cars#my_cars'
 
   resources :cars do
+    collection do
+      get :my_cars
+    end
     resources :bookings, only: [ :new, :create, :index, :show, :destroy ]
   end
-
 
   # All Routes for now untill other models and routes are defined
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
